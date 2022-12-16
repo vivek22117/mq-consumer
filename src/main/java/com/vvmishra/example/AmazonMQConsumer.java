133ae6d087fc750979192ab265b86515b8642c47
vivek22117 <vivekmishra22117@gmail.com>
2022-12-16 14:07:02 +0530

    adding code for ActiveMQ consumer

--- src/main/java/com/vvmishra/example/AmazonMQConsumer.java
+++ src/main/java/com/vvmishra/example/AmazonMQConsumer.java
@@ -6,13 +6,7 @@ import com.rabbitmq.client.ConnectionFactory;
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.jms.pool.PooledConnectionFactory;
 
-import javax.jms.Connection;
-import javax.jms.MessageConsumer;
-import javax.jms.Message;
-import javax.jms.TextMessage;
-import javax.jms.Destination;
-import javax.jms.JMSException;
-import javax.jms.Session;
+import javax.jms.*;
 import java.io.IOException;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
@@ -27,32 +21,69 @@ import java.util.stream.IntStream;
 public class AmazonMQConsumer {
     // Specify the connection parameters.
     private final static String WIRE_LEVEL_ENDPOINT
-            = "ssl://b-a37b689a-d1e9-426d-8415-87e3b4331492-1.mq.ap-south-1.amazonaws.com:61617";
+            = "ssl://b-f56af7c3-9c63-489e-b641-8d4439221c13-1.mq.us-east-1.amazonaws.com:61617";
     private final static String ACTIVE_MQ_USERNAME = "vvmishra";
-    private final static String ACTIVE_MQ_PASSWORD = "Password@2244";
-    private final static String RABBIT_MQ_PASSWORD = "password@2244";
+    private final static String ACTIVE_MQ_PASSWORD = "Password@1234";
+    private final static String RABBIT_MQ_PASSWORD = "password@1234";
 
     public static void main(String[] args) throws JMSException, InterruptedException, NoSuchAlgorithmException, IOException, KeyManagementException, TimeoutException {
-//        final ActiveMQConnectionFactory connectionFactory = createActiveMQConnectionFactory();
-//        final PooledConnectionFactory pooledConnectionFactory = createPooledConnectionFactory(connectionFactory);
-//
-//        receiveMessage(connectionFactory);
-//
-//        pooledConnectionFactory.stop();
+        final ActiveMQConnectionFactory connectionFactory = createActiveMQConnectionFactory();
+        final PooledConnectionFactory pooledConnectionFactory = createPooledConnectionFactory(connectionFactory);
 
-        final ConnectionFactory rabbitMQConnectionFactory = rabbitMQConnectionFactory();
-        IntStream.iterate(0, e -> e +1).limit(7500).forEach(value -> {
-            try {
-                Thread.sleep(1000);
-                publishMessageToRabbitMQ(rabbitMQConnectionFactory, value);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            } catch (TimeoutException e) {
-                throw new RuntimeException(e);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+//        receiveMessage(connectionFactory);
+        sendMessage(connectionFactory);
+
+        pooledConnectionFactory.stop();
+
+//        final ConnectionFactory rabbitMQConnectionFactory = rabbitMQConnectionFactory();
+//        IntStream.iterate(0, e -> e +1).limit(7500).forEach(value -> {
+//            try {
+//                Thread.sleep(1000);
+//                publishMessageToRabbitMQ(rabbitMQConnectionFactory, value);
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            } catch (TimeoutException e) {
+//                throw new RuntimeException(e);
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//        });
+    }
+
+    private static void sendMessage(ActiveMQConnectionFactory connectionFactory) throws JMSException, InterruptedException {
+        // Establish a connection for the consumer.
+        // Note: Consumers should not use PooledConnectionFactory.
+        final Connection producerConnection = connectionFactory.createConnection();
+        producerConnection.start();
+
+        // Create a session.
+        final Session producerSession = producerConnection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        // Create a queue named "MyQueue".
+        final Destination producerDestination = producerSession.createQueue("ttl-testing-queue");
+
+        // Create a message consumer from the session to the queue.
+        final MessageProducer producer = producerSession.createProducer(producerDestination);
+        producer.setTimeToLive(300000);
+        Instant now = Instant.now().plus(30, ChronoUnit.MINUTES);
+
+
+        while (true) {
+            TextMessage textMessage = producerSession.createTextMessage("Hello, I am testing TTL of 5 minutes! " + now.toEpochMilli());
+            producer.send(textMessage);
+
+            Thread.sleep(1000);
+
+            if (now.isBefore(Instant.now())) {
+                break;
             }
-        });
+        }
+
+
+        // Clean up the consumer.
+        producer.close();
+        producerSession.close();
+        producerConnection.close();
     }
 
     private static void receiveMessage(ActiveMQConnectionFactory connectionFactory) throws JMSException, InterruptedException {
@@ -67,21 +98,21 @@ public class AmazonMQConsumer {
 
         // Create a queue named "MyQueue".
         final Destination consumerDestination = consumerSession
-                .createQueue("local-queue");
+                .createQueue("ttl-testing-queue");
         // Create a message consumer from the session to the queue.
         final MessageConsumer consumer = consumerSession
                 .createConsumer(consumerDestination);
-        Instant now = Instant.now().plus(5, ChronoUnit.MINUTES);
+        Instant now = Instant.now().plus(30, ChronoUnit.MINUTES);
 
         while (true) {
 
-            final Message consumerMessage = consumer.receive(1000);
+            final Message consumerMessage = consumer.receive(1);
 
             if (consumerMessage != null) {
                 // Receive the message when it arrives.
                 final TextMessage consumerTextMessage = (TextMessage) consumerMessage;
-                System.out.println("Message received: " + consumerTextMessage.getText());
-                Thread.sleep(2000);
+                System.out.println("Message received at " + now.toEpochMilli() + " --> " + consumerTextMessage.getText());
+                Thread.sleep(10000);
             }
 
 
@@ -117,7 +148,7 @@ public class AmazonMQConsumer {
     }
 
     private static ConnectionFactory rabbitMQConnectionFactory() throws NoSuchAlgorithmException, KeyManagementException, IOException, TimeoutException {
-        ConnectionFactory connectionFactory  = new ConnectionFactory();
+        ConnectionFactory connectionFactory = new ConnectionFactory();
 
         connectionFactory.setUsername(ACTIVE_MQ_USERNAME);
         connectionFactory.setPassword(RABBIT_MQ_PASSWORD);
